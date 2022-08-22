9a510230af51855a518747e2acccd3bee86bf9ed
TAHAR Noureddine <71546633+EngrTAHAR-Noureddine@users.noreply.github.com>
2022-08-22 09:43:47 +0100

    Initial Commit

---
+++ Shared/CustomLockScreenWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLockScreenWidgetApp.swift
+//  Shared
+//
+//  Created by clever zone on 22/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomLockScreenWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
